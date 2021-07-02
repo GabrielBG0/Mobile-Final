e70ae294fc7bef332a1af621d4899a3b8eae01c3
Gabriel Borges Gutierrez <gabriel.bgs00@gmail.com>
2021-07-02 16:16:16 -0300

    tudo - qr code

---
+++ SafeGardMobile/src/Screens/QRCode.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function QRCode(props) {
+  return (
+    <View>
+      <Text>
+        QRCode
+      </Text>
+    </View>
+  )
+}
